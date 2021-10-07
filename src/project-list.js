3526123a4400b503fcc9bb006c6e595af11bca4e
smccourtb <shawnmccourtb@gmail.com>
2021-10-07 10:16:03 -0400

    add comments to project-list.js

--- src/project-list.js
+++ src/project-list.js
@@ -1,10 +1,13 @@
 // used to store all of the individual projects
 import {Project} from "./project.js"
+import {Task} from "./task.js"
+
+// create a housing for all of the projects. Used a module function because we only need one
+// and we need it right away during setup
 
 const ProjectList = (() => {
     let projectList = [];
     const addProject = name => {
-        // const projectToAdd = Project(name);
         projectList.push(name);
     }
     const removeProject = projectName => {
@@ -12,19 +15,23 @@ const ProjectList = (() => {
         projectList.splice(t, 1);
     }
     const defaultListSetup = (() => {
+        // create 3 default projects
         const inbox = Project("Inbox") //default project on load
         const today = Project("Today");
         const upcoming = Project("Upcoming")
         const defaultProjects = [inbox, today, upcoming];
+        // add them to the project list
         for(let project of defaultProjects) {
             addProject(project);
+            // adds an example task to project "Inbox"
+            if (project.getName() === "Inbox") {
+                const newTask = new Task("buy more milk.")
+                project.addTask(newTask)
+            }
         }
-        
     })()
     return {projectList, addProject, removeProject}
 })();
 
-console.log(ProjectList.projectList)
-
-
+console.log(ProjectList)
 export {ProjectList}
