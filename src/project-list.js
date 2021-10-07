0dae91cba532531c6dcd9dc364101a97b3518c66
smccourtb <shawnmccourtb@gmail.com>
2021-10-07 09:43:12 -0400

    add projectList module function to hold all projects

--- src/project-list.js
+++ src/project-list.js
@@ -1,1 +1,16 @@
 // used to store all of the individual projects
+import {Project} from "./project.js"
+
+const ProjectList = (() => {
+    let projectList = [];
+    const addProject = name => {
+        const projectToAdd = Project(name);
+        projectList.push(projectToAdd);
+    }
+    const removeProject = projectName => {
+        const t = projectList.indexOf(projectName);
+        projectList.splice(t, 1);
+    }
+})()
+
+export {ProjectList}
