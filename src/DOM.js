61e574b731a320ad15f28c5adbdccca2db6a1a7c
smccourtb <shawnmccourtb@gmail.com>
2021-10-08 10:18:03 -0400

    add task preview pane and display project title above task input

--- src/DOM.js
+++ src/DOM.js
@@ -1,43 +1,55 @@
-import {Task} from "./task.js"
-import {Project} from "./project.js"
-import {ProjectList} from "./project-list.js"
+import {
+    Task
+} from "./task.js"
+import {
+    Project
+} from "./project.js"
+import {
+    ProjectList
+} from "./project-list.js"
 
 
 
 const showTasks = (project) => {
     const taskList = document.querySelector(".task-list"); // the ol element
     const taskListContainer = document.querySelector(".project-task-list-container")
-    
-    
+    const projectTitle = document.querySelector(".project-title")
+    projectTitle.textContent = project.getName();
     //clear tasks
     taskList.replaceChildren();
     //switch current project to the one that was clicked
-    
+
     for (let task of project.taskList) {
         const checkbox = document.createElement("input"); // checkbox for tasks
         const taskContainer = document.createElement("div"); // houses checkbox and the newLI
         const newLi = document.createElement("li");
         newLi.textContent = task.getTitle();
         taskContainer.classList.add("task-container");
-    checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("type", "checkbox");
         checkbox.addEventListener("click", () => {
-            
+
             if (checkbox.checked) {
                 console.log('hi there');
                 task.markComplete()
                 project.addToCompleted(task)
                 project.removeTask(task)
                 showTasks(project)
-                
+
             }
         })
+        taskContainer.oncontextmenu = (e) => {
+            showTaskMenu(e);
+        }
         taskContainer.append(checkbox, newLi);
 
         taskList.append(taskContainer);
     }
 }
 
-
+const showTaskMenu = (e) => {
+    alert(e);
+    e.preventDefault();
+}
 
 
 
@@ -45,8 +57,8 @@ const setupEventListeners = (() => {
     // setup adding new task to current project
     const setupTaskInputListener = (() => {
         const newTaskInput = document.querySelector(".add-task-input")
-        
-        newTaskInput.addEventListener("keyup", function(event) {
+
+        newTaskInput.addEventListener("keyup", function (event) {
             if (event.key !== "Enter") return;
             const current = ProjectList.getCurrentProject()
             current.addTask(new Task(newTaskInput.value));
@@ -66,20 +78,20 @@ const setupEventListeners = (() => {
             addProjectPopup.classList.toggle("add-project-popup-show");
         })
         // on pressing enter, new project is created then stored in ProjectList
-        addProjectInput.addEventListener("keyup", function(event) {
-            if(event.key !== "Enter") return;
-                // creates a new project then clears input
-                const project = Project(addProjectInput.value);
-                addProjectInput.value = "";
-                // add to ProjectList
-                ProjectList.addProject(project)
-                addProjectPopup.classList.toggle("add-project-popup-show");
-                event.preventDefault();
-                // adds a button to load the project
-                const projectLink = addProjectLink(project);
-                
-                const nav = document.querySelector("nav")
-                nav.append(projectLink)
+        addProjectInput.addEventListener("keyup", function (event) {
+            if (event.key !== "Enter") return;
+            // creates a new project then clears input
+            const project = Project(addProjectInput.value);
+            addProjectInput.value = "";
+            // add to ProjectList
+            ProjectList.addProject(project)
+            addProjectPopup.classList.toggle("add-project-popup-show");
+            event.preventDefault();
+            // adds a button to load the project
+            const projectLink = addProjectLink(project);
+
+            const nav = document.querySelector("nav")
+            nav.append(projectLink)
         })
     })();
 
@@ -100,12 +112,14 @@ const defaultListeventListeners = (() => {
     const todayDOM = document.querySelector(".today")
     const upcomingDOM = document.querySelector(".upcoming")
 
-    for( let i of [inboxDOM, todayDOM, upcomingDOM]) {
+    for (let i of [inboxDOM, todayDOM, upcomingDOM]) {
         i.addEventListener("click", () => {
             ProjectList.setCurrentProject(ProjectList.getProject(i.name))
             showTasks(ProjectList.getProject(i.name))
         })
-    }  
+    }
 })()
 
-export {setupEventListeners}
+export {
+    setupEventListeners
+}
--- src/project-list.js
+++ src/project-list.js
@@ -1,6 +1,10 @@
 // used to store all of the individual projects
-import {Project} from "./project.js"
-import {Task} from "./task.js"
+import {
+    Project
+} from "./project.js"
+import {
+    Task
+} from "./task.js"
 
 // create a housing for all of the projects. Used a module function because we only need one
 // and we need it right away during setup
@@ -22,9 +26,9 @@ const ProjectList = (() => {
         return currentProject;
     }
     const getProject = projectName => {
-        for(let project of ProjectList.projectList) {
-            if(project.getName() === projectName) {
-                return project
+        for (let project of ProjectList.projectList) {
+            if (project.getName() === projectName) {
+                return project;
             }
         };
     }
@@ -35,18 +39,28 @@ const ProjectList = (() => {
         const upcoming = Project("Upcoming")
         const defaultProjects = [inbox, today, upcoming];
         // add them to the project list
-        for(let project of defaultProjects) {
+        for (let project of defaultProjects) {
             addProject(project);
             // adds an example task to project "Inbox"
             if (project.getName() === "Inbox") {
                 const newTask = new Task("buy more milk.")
                 project.addTask(newTask)
             }
-        setCurrentProject(projectList[0])
+            setCurrentProject(projectList[0])
         }
     })()
-    return {currentProject, projectList, addProject, removeProject, getProject, setCurrentProject, getCurrentProject}
+    return {
+        currentProject,
+        projectList,
+        addProject,
+        removeProject,
+        getProject,
+        setCurrentProject,
+        getCurrentProject
+    }
 })();
 
 console.log(ProjectList)
-export {ProjectList}
+export {
+    ProjectList
+}
