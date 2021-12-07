97998446acf748c9bee44ce4dac3464d4936bdad
smccourtb <shawnmccourtb@gmail.com>
2021-12-07 09:58:19 -0500

    setup react and import design and images

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import {
-    loadProject,
-    setupSavedProjects,
-    showTasks
-} from "./DOM.js"
-import {
-    Project
-} from "./project.js";
-import {
-    ProjectList
-} from "./project-list.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import './style.css';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const loadDefault = (showTasks(ProjectList.getCurrentProject()))
-const setup = (setupSavedProjects())
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
