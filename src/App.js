928c770973eb2a27c7a136bd3853a6df65234104
smccourtb <shawnmccourtb@gmail.com>
2021-12-15 11:39:38 -0500

    remove old project files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import './styles/App.css';
-import Task from "./factories/Task";
-import ProjectContainer from './components/ProjectContainer';
-import CreateTask from './components/CreateTask';
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-
-function App() {
-  const [project, setProject] = useState([Task("test 1", uuidv4()), Task("test 2", uuidv4()), Task("test 3", uuidv4())]);
-  
-  
-  return (
-    <div className="App">
-      <CreateTask setProject={setProject}/>
-      <ProjectContainer project={project} setProject={setProject}/>
-      <p>Drag and drop to reorder list</p>
-    </div>
-  );
-}
-
-export default App;
-=======
 import ProjectContainer from "./ProjectContainer";
 import Header from "./Header";
 import "./index.css"
@@ -49,4 +26,3 @@ function App() {
 }
 
 export default App;
->>>>>>> cb8f6ec (transfer project into this repository)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-=======
 import App from "./App";
 import ReactDOM from "react-dom";
 
@@ -20,4 +6,3 @@ const rootElement = document.getElementById("root");
 
 
 ReactDOM.render(<App />, rootElement);
->>>>>>> cb8f6ec (transfer project into this repository)
